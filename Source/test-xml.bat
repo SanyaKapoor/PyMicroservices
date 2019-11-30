ca0b59595398bb9dd5f993f25a8fc9f4d9fa99c1
SanyaKapoor <31134079+SanyaKapoor@users.noreply.github.com>
2019-11-30 11:32:05 +0530

    Update test-xml.bat

--- Source/test-xml.bat
+++ Source/test-xml.bat
@@ -1,6 +1,6 @@
 SET PATH=%PATH%; C:\Python27\;C:\Python27\Scripts
-del "D:\Moodle\Sem 5\Cloud\PyMicroservices-master\Source\python_tests_xml\*.*"
-del "D:\Moodle\Sem 5\Cloud\PyMicroservices-master\Source\coverage.xml"
+REM del "D:\Moodle\Sem 5\Cloud\PyMicroservices-master\Source\python_tests_xml\*.*"
+REM del "D:\Moodle\Sem 5\Cloud\PyMicroservices-master\Source\coverage.xml"
 cd "D:\Moodle\Sem 5\Cloud\PyMicroservices-master\Source\coverage.xml"
 REM C:\Python27\python runtests.py
 REM C:\Python27\python -m coverage run -m pytest && coverage xml -o coverage.xml
