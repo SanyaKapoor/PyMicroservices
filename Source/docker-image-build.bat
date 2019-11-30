46e41d1f004072052673235769f02f04dc2657bc
SanyaKapoor <31134079+SanyaKapoor@users.noreply.github.com>
2019-11-30 16:47:18 +0530

    Update docker-image-build.bat

--- Source/docker-image-build.bat
+++ Source/docker-image-build.bat
@@ -2,3 +2,4 @@ SET PATH=%PATH%;"C:\Users\Sanya Kapoor\AppData\Local\Google\Cloud SDK\google-clo
 SET IMAGE_NAME=gcr.io/compile-microservices/gpymicro-image:%BUILD_NUMBER%
 cd "D:\Moodle\Sem 5\Cloud\PyMicroservices-master\Source"
 gcloud builds submit --tag %IMAGE_NAME% .
+python main.py %BUILD_NUMBER%
