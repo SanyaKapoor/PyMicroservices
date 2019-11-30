dc029ad1cd9dfea0adb3bd617188bb898306b3eb
SanyaKapoor <31134079+SanyaKapoor@users.noreply.github.com>
2019-11-30 12:21:03 +0530

    Update pre-build-config.bat

--- Source/pre-build-config.bat
+++ Source/pre-build-config.bat
@@ -1,3 +1,3 @@
 SET PATH=%PATH%;"C:\Users\Sanya Kapoor\AppData\Local\Google\Cloud SDK\google-cloud-sdk\bin"
-git pull
+git pull "https://github.com/SanyaKapoor/PyMicroservices.git"
 gcloud auth activate-service-account "tests-454@compile-microservices.iam.gserviceaccount.com" --key-file="D:\Moodle\Sem 5\Cloud\PyMicroservices-master\Source\compile-microservices-83df407d88bd.json" --project=compile-microservices
