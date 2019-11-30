9d0e97e03381457a2da4bec02e990c6f42ada5ae
SanyaKapoor <31134079+SanyaKapoor@users.noreply.github.com>
2019-11-30 12:05:04 +0530

    Add files via upload

--- Source/test-xml.bat
+++ Source/test-xml.bat
@@ -1,8 +1,5 @@
 SET PATH=%PATH%; C:\Python27\;C:\Python27\Scripts
-cd "D:\Moodle\Sem 5\Cloud\PyMicroservices-master\Source\coverage.xml"
-REM C:\Python27\python runtests.py
-REM C:\Python27\python -m coverage run -m pytest && coverage xml -o coverage.xml
-REM C:\Python27\python -m coverage html -d coverage
+cd "D:\Moodle\Sem 5\Cloud\PyMicroservices-master\Source"
 python runtests.py
 python -m coverage run -m pytest && coverage xml -o coverage.xml
 copy "D:\Moodle\Sem 5\Cloud\PyMicroservices-master\Source\python_tests_xml\*.*" "D:\Moodle\Sem 5\Cloud\PyMicroservices-master\*.*"
