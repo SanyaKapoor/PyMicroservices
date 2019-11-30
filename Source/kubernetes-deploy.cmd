073b554ca2c4958ca30223b2df98c5b706d0d122
SanyaKapoor <31134079+SanyaKapoor@users.noreply.github.com>
2019-11-30 16:37:08 +0530

    Update kubernetes-deploy.cmd

--- Source/kubernetes-deploy.cmd
+++ Source/kubernetes-deploy.cmd
@@ -1,4 +1,2 @@
-SET PATH=%PATH%;"C:\Users\Sanya Kapoor\AppData\Local\Google\Cloud SDK\google-cloud-sdk\bin"
-SET IMAGE_NAME=gcr.io/compile-microservices/gpymicro-image:%BUILD_NUMBER%
 cd "D:\Moodle\Sem 5\Cloud\PyMicroservices-master\Source"
-exec "kubectl set image deployment/py-gmicro py-gmicro=%IMAGE_NAME%"
+python main.py %BUILD_NUMBER%
