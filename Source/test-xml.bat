2c122a31f61087989d26b1ee7aab36d2d920bf33
SanyaKapoor <31134079+SanyaKapoor@users.noreply.github.com>
2019-11-30 11:28:44 +0530

    Update test-xml.bat

--- Source/test-xml.bat
+++ Source/test-xml.bat
@@ -1,11 +1,11 @@
-SET PATH=%PATH%;C:\Python27\;C:\Python27\Scripts
-del D:\Moodle\Sem 5\Cloud\PyMicroservices-master\Source\python_tests_xml\*.*
-del D:\Moodle\Sem 5\Cloud\PyMicroservices-master\Source\coverage.xml
-cd D:\Moodle\Sem 5\Cloud\PyMicroservices-master\Source\coverage.xml
+SET PATH=%PATH%; C:\Python27\;C:\Python27\Scripts
+del "D:\Moodle\Sem 5\Cloud\PyMicroservices-master\Source\python_tests_xml\*.*"
+del "D:\Moodle\Sem 5\Cloud\PyMicroservices-master\Source\coverage.xml"
+cd "D:\Moodle\Sem 5\Cloud\PyMicroservices-master\Source\coverage.xml"
 REM C:\Python27\python runtests.py
 REM C:\Python27\python -m coverage run -m pytest && coverage xml -o coverage.xml
 REM C:\Python27\python -m coverage html -d coverage
 python runtests.py
 python -m coverage run -m pytest && coverage xml -o coverage.xml
-copy D:\Moodle\Sem 5\Cloud\PyMicroservices-master\Source\python_tests_xml\*.* D:\Moodle\Sem 5\Cloud\PyMicroservices-master\python_tests_xml\*.*
-copy D:\Moodle\Sem 5\Cloud\PyMicroservices-master\Source\coverage.xml D:\Moodle\Sem 5\Cloud\PyMicroservices-master\coverage.xml
+copy "D:\Moodle\Sem 5\Cloud\PyMicroservices-master\Source\python_tests_xml\*.*" "D:\Moodle\Sem 5\Cloud\PyMicroservices-master\*.*"
+copy "D:\Moodle\Sem 5\Cloud\PyMicroservices-master\Source\coverage.xml" "D:\Moodle\Sem 5\Cloud\PyMicroservices-master\coverage.xml"
